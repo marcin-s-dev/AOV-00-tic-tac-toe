4e7d72c55a1d24c5145c6d3b9b6f94f32d71a4dc
marcin.s.dev <118073720+marcin-s-dev@users.noreply.github.com>
2022-11-13 19:00:17 +0100

    feat: implement mvp

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,17 +1,87 @@
 <template>
-  <div class="grid grid-rows-3 grid-cols-3" data-test="grid">
-    <div data-test="grid-child" class="border border-green h-32 w-32"></div>
-    <div data-test="grid-child" class="border border-green h-32 w-32"></div>
-    <div data-test="grid-child" class="border border-green h-32 w-32"></div>
-    <div data-test="grid-child" class="border border-green h-32 w-32"></div>
-    <div data-test="grid-child" class="border border-green h-32 w-32"></div>
-    <div data-test="grid-child" class="border border-green h-32 w-32"></div>
-    <div data-test="grid-child" class="border border-green h-32 w-32"></div>
-    <div data-test="grid-child" class="border border-green h-32 w-32"></div>
-    <div data-test="grid-child" class="border border-green h-32 w-32"></div>
+  <div class="flex flex-col">
+    <div class="grid grid-rows-3 grid-cols-3 overflow-hidden" data-test="grid">
+      <div
+        v-for="cell in gameGridCells"
+        :key="cell.idx"
+        data-test="grid-child"
+        class="outline outline-green h-20 md:h-32 lg:h-44 w-20 md:w-32 lg:w-44 text-6xl md:text-9xl grid place-content-center"
+        @click="placeMark(cell.idx, player)"
+      >
+        {{ cell.sign }}
+      </div>
+    </div>
+    <span v-if="!isGameOver" data-test="current-player" class="text-4xl mt-4">{{ player }} turn</span>
+    <span v-if="isGameOver" data-test="game-over" class="text-4xl mt-4">Game Over!</span>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed, Ref, ref } from 'vue';
+type Player = 'O' | 'X';
+interface GridCell {
+  idx: number;
+  sign: string;
+}
+
+const gameGridCells: Ref<GridCell[]> = ref([
+  { idx: 0, sign: '' },
+  { idx: 1, sign: '' },
+  { idx: 2, sign: '' },
+  { idx: 3, sign: '' },
+  { idx: 4, sign: '' },
+  { idx: 5, sign: '' },
+  { idx: 6, sign: '' },
+  { idx: 7, sign: '' },
+  { idx: 8, sign: '' },
+]);
+
+const player: Ref<Player> = ref('O');
+const isGameOver = computed(
+  () => hasWinner(gameGridCells.value) || gameGridCells.value.every(cell => cell.sign !== '')
+);
+
+const placeMark = (idx: number, sign: Player): void => {
+  if (isGameOver.value) {
+    return;
+  }
+  const targetCell = gameGridCells.value.find(cell => cell.idx === idx);
+  if (targetCell?.sign !== '') {
+    return;
+  }
+
+  gameGridCells.value = gameGridCells.value.map(cell => (cell.idx === idx ? { ...cell, sign: sign } : cell));
+  switchPlayerTurns();
+};
+
+const switchPlayerTurns = (): void => {
+  if (player.value === 'O') {
+    player.value = 'X';
+    return;
+  }
+
+  player.value = 'O';
+};
+
+const hasWinner = (gameGridCells: GridCell[]) => {
+  return (
+    isWinner(gameGridCells[0], gameGridCells[1], gameGridCells[2]) || // check for 3-in-a-row horizontally
+    isWinner(gameGridCells[3], gameGridCells[4], gameGridCells[5]) ||
+    isWinner(gameGridCells[6], gameGridCells[7], gameGridCells[8]) ||
+    isWinner(gameGridCells[0], gameGridCells[3], gameGridCells[6]) || // check for 3-in-a-row vertically
+    isWinner(gameGridCells[1], gameGridCells[4], gameGridCells[7]) ||
+    isWinner(gameGridCells[2], gameGridCells[5], gameGridCells[8]) ||
+    isWinner(gameGridCells[0], gameGridCells[4], gameGridCells[8]) || // check for 3-in-a-row diagonally
+    isWinner(gameGridCells[6], gameGridCells[4], gameGridCells[2])
+  );
+};
+
+const isWinner = (p1: GridCell, p2: GridCell, p3: GridCell) => {
+  if (!p1.sign || !p2.sign || !p3.sign) {
+    return false;
+  }
+  return p1.sign === p2.sign && p2.sign === p3.sign;
+};
+</script>
 
 <style></style>
--- src/tests/game.test.ts
+++ src/tests/game.test.ts
@@ -1,16 +1,82 @@
 import { beforeEach, describe, expect, it } from 'vitest';
 import { mount, VueWrapper } from '@vue/test-utils';
 import Game from '../components/Game.vue';
+
+const placeMark = async (wrapper, childIndex) => {
+  const grid = wrapper.find('[data-test="grid"]');
+  const children = grid.findAll('[data-test="grid-child"]');
+  const child = children[childIndex];
+  await child.trigger('click');
+  return child;
+};
+
 describe('Game', () => {
   let wrapper: VueWrapper;
   beforeEach(() => {
     wrapper = mount(Game);
   });
-  it('renders a 3x3 grid', async () => {
+  it('renders a 3x3 grid', () => {
     const grid = wrapper.find('[data-test="grid"]');
+    expect(grid.classes()).toContain('grid');
     expect(grid.classes()).toContain('grid-rows-3');
     expect(grid.classes()).toContain('grid-cols-3');
     const children = grid.findAll('[data-test="grid-child"]');
     expect(children.length).toBe(9);
+  });
+
+  it('renders the corrent sign when cell is clicked', async () => {
+    const markedFirstChild = await placeMark(wrapper, 0);
+    expect(markedFirstChild.text()).toBe('O');
+
+    const markedSecondChild = await placeMark(wrapper, 1);
+    expect(markedSecondChild.text()).toBe('X');
+  });
+
+  it('renders initial player turn information', async () => {
+    //assuming O will always start first
+    const playerTurnInfo = wrapper.find('[data-test="current-player"]');
+    expect(playerTurnInfo.text()).to.eq('O turn');
+  });
+
+  it('renders a correct player turn information', async () => {
+    //assuming O will always start first
+    await placeMark(wrapper, 0);
+
+    const playerTurnInfo = wrapper.find('[data-test="current-player"]');
+    expect(playerTurnInfo.text()).to.eq('X turn');
+  });
+
+  it('does not allow to place a sign on a taken position', async () => {
+    //assuming O will always start first
+    const markedFirstChild = await placeMark(wrapper, 0);
+    expect(markedFirstChild.text()).toBe('O');
+    const remarkedFirstChild = await placeMark(wrapper, 0);
+    expect(remarkedFirstChild.text()).toBe('O');
+  });
+
+  it('does not allow to place a sign when the game is over', async () => {
+    //make "O" win
+    await placeMark(wrapper, 0);
+    await placeMark(wrapper, 4);
+    await placeMark(wrapper, 1);
+    await placeMark(wrapper, 5);
+    await placeMark(wrapper, 2);
+
+    //try to place a new mark
+    const marked = await placeMark(wrapper, 6);
+    expect(marked.text()).toBe('');
+  });
+
+  it('renders a game over text', async () => {
+    //make "O" win
+    await placeMark(wrapper, 0);
+    await placeMark(wrapper, 4);
+    await placeMark(wrapper, 1);
+    await placeMark(wrapper, 5);
+    await placeMark(wrapper, 2);
+
+    //try to place a new mark
+    const gameOverElement = wrapper.find('[data-test="game-over"]');
+    expect(gameOverElement.text()).toBe('Game Over!');
   });
 });
